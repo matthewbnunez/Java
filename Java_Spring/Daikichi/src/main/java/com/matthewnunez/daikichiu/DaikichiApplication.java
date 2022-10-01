b06e9b2fbd7e33002d87ffd3ca734a1014d883d2
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-09-30 21:50:50 -0700

    initial commit

---
+++ Java_Spring/Daikichi/src/main/java/com/matthewnunez/daikichiu/DaikichiApplication.java
@@ -0,0 +1,12 @@
+package com.matthewnunez.daikichiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiApplication.class, args);
+	}
+}
---
+++ Java_Spring/Daikichi/src/main/java/com/matthewnunez/daikichiu/HomeController.java
@@ -0,0 +1,23 @@
+package com.matthewnunez.daikichiu;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class HomeController {
+    @RequestMapping("")
+    public String welcome() {
+            return "Welcome!";
+    }
+    
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all you endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+}
