5f8a0e3dbb778f979e9938f26d6f7e2ad3b36ad2
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-09-30 22:20:02 -0700

    added RequestParam and PathVariables

--- Java_Spring/Daikichi/src/main/java/com/matthewnunez/daikichiu/HomeController.java
+++ Java_Spring/Daikichi/src/main/java/com/matthewnunez/daikichiu/HomeController.java
@@ -1,7 +1,8 @@
 package com.matthewnunez.daikichiu;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/daikichi")
@@ -19,5 +20,23 @@ public class HomeController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel")
+	public String travel(@RequestParam(value="1", required=false) String searchQuery) {
+		if(searchQuery == null) {
+			return "Wrong Url";
+		} else {
+			return "Congratulations! You will soo travel to " + searchQuery + "!";
+		}
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if(num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
